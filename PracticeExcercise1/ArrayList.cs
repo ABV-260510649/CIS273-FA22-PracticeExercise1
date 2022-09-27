9a62cbf10fcac95e47097b274c78a0a5ac8c1088
Abbie Vance <abbie.vance@students.fhu.edu>
2022-09-26 21:34:50 -0500

    round two of trying to push / pull without merge problems

--- PracticeExcercise1/ArrayList.cs
+++ PracticeExcercise1/ArrayList.cs
@@ -1,43 +1,61 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+
 namespace PracticeExercise1
 {
-	public class ArrayList : IList
-	{
+    public class ArrayList : IList
+    {
         private int[] array;
         private int length;
 
-		public ArrayList()
-		{
+        public ArrayList()
+        {
             array = new int[16];
             length = 0;
-		}
+        }
 
+        // TODO
         /// <summary>
         /// Returns first element in list, null if empty.
         /// </summary>
         public int? First
         {
-            //get
-            //{
-            //    if (IsEmpty)
-            //    {
-            //        return null;
-            //    }
-            //    else
-            //    {
-            //        return array[0];
-            //    }
-            //}
-
-            get => IsEmpty ? null : array[0];
+            get
+            {
+                if (IsEmpty)
+                {
+                    return null;
+                }
+                else
+                {
+                    return array[0];
+                }
+            }
         }
 
         // TODO
         /// <summary>
         /// Returns last element in list, null if empty.
         /// </summary>
-        public int? Last { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int? Last
+        {
+            get
+            {
+                if (IsEmpty)
+                {
+                    return null;
+                }
+                else
+                {
+                    return array[length - 1];
+                }
+            }
+        }
 
+        // TODO
         /// <summary>
         /// Returns true if list is has no elements; false otherwise.
         /// </summary>
@@ -55,11 +73,10 @@ namespace PracticeExercise1
         /// <param name="value"></param>
         public void Append(int value)
         {
-            if(Length == array.Length)
+            if (length == 0 || IsNotFull(value) == false)
             {
                 Resize();
             }
-
             array[length] = value;
             length++;
         }
@@ -71,9 +88,9 @@ namespace PracticeExercise1
         /// <returns>true if value is in list; false otherwise</returns>
         public bool Contains(int value)
         {
-            for(int i=0; i < Length; i++)
+            for (int i = 0; i < Length; i++)
             {
-                if( array[i] == value)
+                if (array[i] == value)
                 {
                     return true;
                 }
@@ -90,7 +107,14 @@ namespace PracticeExercise1
         /// <returns>Index of first element with value; -1 if element is not found</returns>
         public int FirstIndexOf(int value)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < length; i++)
+            {
+                if (array[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         // TODO
@@ -102,7 +126,19 @@ namespace PracticeExercise1
         /// <param name="existingValue"></param>
         public void InsertAfter(int newValue, int existingValue)
         {
-            throw new NotImplementedException();
+            if (FirstIndexOf(existingValue) < 0)
+            {
+                Append(newValue);
+            }
+
+            else if (FirstIndexOf(existingValue) >= 0)
+            {
+                ShiftRight(FirstIndexOf(existingValue));
+                array[FirstIndexOf(existingValue) + 1] = newValue;
+                length++;
+            }
+
+
         }
 
         // TODO
@@ -113,7 +149,14 @@ namespace PracticeExercise1
         /// <param name="index"></param>
         public void InsertAt(int value, int index)
         {
-            throw new NotImplementedException();
+            if (index > length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            ShiftRight(index);
+            array[index] = value;
+            length++;
         }
 
         /// <summary>
@@ -122,11 +165,10 @@ namespace PracticeExercise1
         /// <param name="value"></param>
         public void Prepend(int value)
         {
-            if (Length == array.Length)
+            if (length == 0 || IsNotFull(value) == false)
             {
                 Resize();
             }
-
             // shift elements to right
             ShiftRight(0);
 
@@ -137,20 +179,19 @@ namespace PracticeExercise1
 
         private void ShiftRight(int index)
         {
-            for(int i = Length-1; i >= index; i--)
+            for (int i = Length - 1; i >= index; i--)
             {
                 array[i + 1] = array[i];
             }
         }
 
-        private void ShiftLeft( int startingIndex)
+        private void ShiftLeft(int startingIndex)
         {
-            for(int i = startingIndex+1; i <= Length; i++ )
+            for (int i = startingIndex + 1; i < Length; i++)
             {
-                array[i-1] = array[i];
+                array[i - 1] = array[i];
             }
         }
-
 
         // TODO
         /// <summary>
@@ -159,7 +200,10 @@ namespace PracticeExercise1
         /// <param name="value">value of item to be removed</param>
         public void Remove(int value)
         {
-            throw new NotImplementedException();
+            if (array.Contains(value) != false && IsEmpty == false)
+            {
+                RemoveAt(FirstIndexOf(value));
+            }
         }
 
         // TODO
@@ -169,42 +213,48 @@ namespace PracticeExercise1
         /// <param name="index"></param>
         public void RemoveAt(int index)
         {
-            ShiftLeft(index);
-            length--;
-        }
-
-        public override string ToString()
-        {
-            string str = "[ ";
-
-            for(int i=0; i < Length-1; i++)
-            {
-                str += array[i] + ", ";
-            }
-
-            str += array[Length - 1];
-            str += "]";
-
-            return str;
-
-        }
-
-        /// <summary>
-        /// Return the element at the given index.
-        /// </summary>
-        /// <param name="index"></param>
-        /// <returns>The element at the given index.</returns>
-        public int Get(int index)
-        {
-            if(index > Length - 1 || index < 0)
+            if (index >= Length || index < 0)
             {
                 throw new IndexOutOfRangeException();
             }
+            else
+            {
+                ShiftLeft(index);
+                length--;
+            }
+        }
 
+        // TODO
+        // String returns "[item1, item2, item3]"
+        public override string ToString()
+        {
+            string str = "[";
+            for (int i = 0; i < Length - 1; i++)
+            {
+                str += array[i] + ", ";
+            }
+            if (length > 0)
+            {
+                str += array[Length - 1];
+            }
+            str += "]";
+            return str;
+        }
+
+        /// <summary>
+        /// Return the element at the given index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>The element at the given index</returns>
+
+        public int Get(int index)
+        {
+            if (index > Length - 1 || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
             int element = array[index];
-
             return element;
-
         }
 
         /// <summary>
@@ -215,28 +265,29 @@ namespace PracticeExercise1
             length = 0;
         }
 
-        /// <summary>
-        /// Return a new copy of list in reverse order
-        /// </summary>
-        /// <returns></returns>
         public IList Reverse()
         {
             IList reverse = new ArrayList();
-
-            for(int i = 0; i < Length; i++)
+            for (int i = 0; i < Length; i++)
             {
                 reverse.Prepend(array[i]);
             }
-
             return reverse;
         }
-
 
         private void Resize()
         {
             Array.Resize(ref array, 2 * array.Length);
         }
 
+        private bool IsNotFull(int index)
+        {
+            if (index > 0 && index < array.Length)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
 
